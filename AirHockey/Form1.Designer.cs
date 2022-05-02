cabd9719491f5141537499f835aa44436b59135d
Brianna312 <briahutc312@ed.amdsb.ca>
2022-05-02 14:25:31 -0400

    Add project files.

---
+++ AirHockey/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AirHockey
+{
+    partial class AirHockey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameEngine = new System.Windows.Forms.Timer(this.components);
+            this.countdown = new System.Windows.Forms.Label();
+            this.secret = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameEngine
+            // 
+            this.gameEngine.Interval = 20;
+            this.gameEngine.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // countdown
+            // 
+            this.countdown.AutoSize = true;
+            this.countdown.Font = new System.Drawing.Font("MV Boli", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdown.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.countdown.Location = new System.Drawing.Point(300, 297);
+            this.countdown.Name = "countdown";
+            this.countdown.Size = new System.Drawing.Size(62, 65);
+            this.countdown.TabIndex = 0;
+            this.countdown.Text = "3";
+            this.countdown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdown.Visible = false;
+            // 
+            // secret
+            // 
+            this.secret.AutoSize = true;
+            this.secret.Location = new System.Drawing.Point(572, 26);
+            this.secret.Name = "secret";
+            this.secret.Size = new System.Drawing.Size(44, 16);
+            this.secret.TabIndex = 1;
+            this.secret.Text = "label1";
+            this.secret.TextChanged += new System.EventHandler(this.secret_TextChanged);
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(603, 9);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(45, 45);
+            this.p1ScoreLabel.TabIndex = 2;
+            this.p1ScoreLabel.Text = "0";
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(12, 766);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(45, 45);
+            this.p2ScoreLabel.TabIndex = 3;
+            this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(192, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // AirHockey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(659, 820);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.secret);
+            this.Controls.Add(this.countdown);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AirHockey";
+            this.Text = "Air Hockey";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.AirHockey_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AirHockey_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.AirHockey_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameEngine;
+        private System.Windows.Forms.Label countdown;
+        private System.Windows.Forms.Label secret;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AirHockey/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AirHockey
+{
+    public partial class AirHockey : Form
+    {
+        /// <summary>
+        /// form1.Width = 510;
+        ///     form1.width/2 = 255.5;
+        ///                    - 80.0
+        ///                     175.5
+        ///                     
+        /// form1.height = 705;
+        ///     Form1.height/2 = 352.5;
+        ///                      352
+        /// </summary>
+
+        Rectangle player1 = new Rectangle(235, 30, 40, 40);
+        Rectangle player2 = new Rectangle(235, 595, 40, 40);
+        Rectangle ball = new Rectangle(248, 345, 15, 15);
+
+        Rectangle bottom = new Rectangle(0, 670, 510, 1);
+        Rectangle top = new Rectangle(0, 0, 510, 1);
+        Rectangle left = new Rectangle(-10, -10, 10, 705);
+        Rectangle right = new Rectangle(500, -10, 10, 705);
+
+        Rectangle p1Goal = new Rectangle(175, 1, 160, 2);
+        Rectangle p2Goal = new Rectangle(176, 665, 160, 2);
+
+
+        Brush whiteBrush = new SolidBrush(Color.White);
+        Pen whitePen = new Pen(Color.White, 2);
+
+        int p1Score = 0;
+        int p2Score = 0;
+        int pXSpeed = 5;
+        int pYSpeed = 4;
+
+        int ballXSpeed = 8;
+        int ballYSpeed = 6;
+
+
+        bool wDown = false;
+        bool aDown = false;
+        bool sDown = false;
+        bool dDown = false;
+
+        bool upArrowDown = false;
+        bool leftArrowDown = false;
+        bool downArrowDown = false;
+        bool rightArrowDown = false;
+
+        bool countingDown = true;
+
+        Random random = new Random();
+        public AirHockey()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            ////Moving the players
+            if (countingDown == false)
+            {
+                p2ScoreLabel.Text = $"{p2Score}";
+                p1ScoreLabel.Text = $"{p1Score}";
+
+                if (wDown == true && player1.Y > 0)
+                {
+                    player1.Y -= pYSpeed;
+                }
+                else if (sDown == true && player1.Y < this.Height - 80)
+                {
+                    player1.Y += pYSpeed;
+                }
+
+                if (upArrowDown == true && player2.Y > 0)
+                {
+                    player2.Y -= pYSpeed;
+                }
+                else if (downArrowDown == true && player2.Y < this.Height - 80)
+                {
+                    player2.Y += pYSpeed;
+                }
+
+
+                if (aDown == true && player1.X > 0)
+                {
+                    player1.X -= pXSpeed;
+                }
+                else if (dDown == true && player1.X < this.Width - 60)
+                {
+                    player1.X += pXSpeed;
+                }
+
+                if (leftArrowDown == true && player2.X > 0)
+                {
+                    player2.X -= pXSpeed;
+                }
+                else if (rightArrowDown == true && player2.X < this.Width - 60)
+                {
+                    player2.X += pXSpeed;
+                }
+
+                ////moving the ball
+                ball.X += ballXSpeed;
+                ball.Y += ballYSpeed;
+                ///intersecting players
+                if (ball.IntersectsWith(player1) && player1.Y > ball.Y || ball.IntersectsWith(player1) && player1.Y < ball.Y ||
+                   ball.IntersectsWith(player2) && player1.Y > ball.Y || ball.IntersectsWith(player2) && player1.Y < ball.Y)
+                {
+                    ballYSpeed *= -1;
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+
+                }
+                if (ball.IntersectsWith(player1) && player1.X > ball.X || ball.IntersectsWith(player1) && player1.X < ball.X ||
+                   ball.IntersectsWith(player2) && player1.X > ball.X || ball.IntersectsWith(player2) && player1.X < ball.X)
+                {
+                    ballXSpeed *= -1;
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+
+                }
+                ///intersecting walls
+                ///
+                if (ball.IntersectsWith(bottom))
+                {
+                    ballYSpeed = random.Next(-15, -5);
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+                }
+                else if (ball.IntersectsWith(top) || top.IntersectsWith(ball))
+                {
+                    ballYSpeed = random.Next(5, 15);
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+                }
+                if (ball.IntersectsWith(left))
+                {
+                    ballXSpeed = random.Next(6, 19);
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+                }
+                else if (ball.IntersectsWith(right))
+                {
+                    ballXSpeed = random.Next(-19, -6);
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.hit);
+                    sound.Play();
+                }
+                ///Scoring goals
+                if (ball.IntersectsWith(p1Goal))
+                {
+
+                    p2Score++;
+                    player1.Location = new Point(235, 30);
+                    player2.Location = new Point(235, 595);
+                    ball.Location = new Point(248, 345);
+                    p2ScoreLabel.Text = $"{p2Score}";
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.RPReplay_Final1651259595);
+                    sound.Play();
+                    if (p2Score == 3)
+                    {
+                        countdown.Text = "Player 2 wins!";
+                        countdown.Visible = true;
+                        countdown.Location = new Point(this.Width / 2 - countdown.Width / 2, this.Height / 2 - countdown.Height / 2);
+                        gameEngine.Enabled = false;
+
+                        button1.Visible = true;
+                        button1.Location = new Point(this.Width / 2 - button1.Width, 400);
+                    }
+                    else
+                    {
+                        countingDown = true;
+                        secret.Text = "p1Goal";
+                    }
+                }
+                else if (ball.IntersectsWith(p2Goal))
+                {
+                    p1Score++;
+                    player1.Location = new Point(235, 30);
+                    player2.Location = new Point(235, 595);
+                    ball.Location = new Point(248, 345);
+                    p1ScoreLabel.Text = $"{p1Score}";
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.RPReplay_Final1651259595);
+                    sound.Play();
+                    if (p1Score == 3)
+                    {
+                        countdown.Text = "Player 1 wins!";
+                        countdown.Visible = true;
+                        countdown.Location = new Point(this.Width / 2 - countdown.Width / 2, this.Height / 2 - countdown.Height / 2);
+                        gameEngine.Enabled = false;
+
+                        button1.Location = new Point(this.Width / 2 - button1.Width / 2, 400);
+                        button1.Visible = true;
+                    }
+                    else
+                    {
+                        countingDown = true;
+                        secret.Text = "p2Goal";
+                    }
+
+                }
+                else
+                {
+                    secret.Text = "playing";
+                }
+
+            }
+
+            Refresh();
+        }
+
+        private void AirHockey_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+            }
+
+        }
+
+        private void AirHockey_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+            }
+        }
+
+        public void Countdown()
+        {
+            countdown.Visible = true;
+            countdown.Text = "3";
+            countdown.Location = new Point(this.Width / 2 - countdown.Width / 2, 200);            
+            Refresh();
+            countdown.Text = "2";
+            countdown.Location = new Point(this.Width / 2 - countdown.Width / 2, 200);
+            Thread.Sleep(800);
+            Refresh();
+            countdown.Text = "1";
+            countdown.Location = new Point(this.Width / 2 - countdown.Width / 2, 200);
+            Thread.Sleep(800);
+            Refresh();
+
+            ballXSpeed = 8;
+            ballXSpeed *= random.Next(-1, 2);
+            while (ballXSpeed == 0)
+            {
+                ballXSpeed = 8;
+                ballXSpeed *= random.Next(-1, 2);
+            }
+
+            ballYSpeed = 6;
+            ballYSpeed *= random.Next(-1, 2);
+            while (ballYSpeed == 0)
+            {
+                ballYSpeed = 6;
+                ballYSpeed *= random.Next(-1, 2);
+            }
+            Thread.Sleep(800);
+            Refresh();
+
+            countdown.Visible = false;
+            countingDown = false;            
+            Refresh();
+
+        }
+        private void AirHockey_Paint(object sender, PaintEventArgs e)
+        {
+
+            e.Graphics.FillEllipse(whiteBrush, player1);
+            e.Graphics.FillEllipse(whiteBrush, player2);
+
+            e.Graphics.FillEllipse(whiteBrush, ball);
+
+            e.Graphics.DrawEllipse(whitePen, this.Width / 2 - 80, this.Height / 2 - 80, 160, 160);
+
+            //Goal circle thingies
+            e.Graphics.DrawEllipse(whitePen, this.Width / 2 - 80, -80, 160, 130);
+            e.Graphics.DrawEllipse(whitePen, this.Width / 2 - 80, this.Height - 90, 160, 130);
+
+            //e.Graphics.DrawLine(whitePen, 0, this.Height / 2, this.Width, this.Height / 2);
+            //e.Graphics.DrawLine(whitePen, this.Width / 2, 0, this.Width / 2, this.Height);
+
+            //goals
+            e.Graphics.FillRectangle(whiteBrush, p1Goal);
+            e.Graphics.FillRectangle(whiteBrush, p2Goal);
+        }
+
+
+        private void secret_TextChanged(object sender, EventArgs e)
+        {
+            if (secret.Text == "playing")
+            {
+
+            }
+            else
+            {
+                Countdown();
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            gameEngine.Enabled = true;
+            gameEngine.Start();
+            p1Score = 0;
+            p2Score = 0;
+            p1ScoreLabel.Text = $"{p1Score}";
+            p2ScoreLabel.Text = $"{p2Score}";
+            button1.Visible = false;
+            button1.Text = "PLAY AGAIN?";
+            Countdown();
+        }
+    }
+}
+
+        
